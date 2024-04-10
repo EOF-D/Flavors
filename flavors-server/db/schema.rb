30ed2887841c67c9e4939159595d0f9b8fb5d193
EOF-D <END-OFD@pm.me>
2024-04-10 19:12:58 -0400

    fix: Updated dead schema

--- flavors-server/db/schema.rb
+++ flavors-server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_09_144429) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_10_231005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_09_144429) do
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "password_digest", null: false
+    t.text "saved_recipes", default: [], array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
